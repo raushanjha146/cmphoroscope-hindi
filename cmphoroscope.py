f22cb097f53ab9a9eeaf49f8d5fb5e13be6c7248
Raushan Kumar <raushanjha146@gmail.com>
2019-01-02 18:00:31 +0530

    update cmphoroscope.py

--- cmphoroscope.py
+++ cmphoroscope.py
@@ -28,7 +28,19 @@ class CMPHoroscope:
         #print("-4-->" + date)
 
         ######### fro Horoscope ################
-        web_page = urlopen(url_hindi)
+        hdr = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+            'Accept-Encoding': 'none',
+            'Accept-Language': 'en-US,en;q=0.8',
+            'Connection': 'keep-alive'}
+        req = urllib2.Request(url_hindi, headers=hdr)
+        try:
+            web_page = urllib2.urlopen(req)
+        except urllib2.HTTPError, e:
+            print "--------------" +e.fp.read()
+		#web_page = urlopen(url_hindi)
         soup = BeautifulSoup(web_page, 'html.parser')
         #print "---TODAY---"
         for extract_div in soup.findAll("div", {"id": "today1"}):
